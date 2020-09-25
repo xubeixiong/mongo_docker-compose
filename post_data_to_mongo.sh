e0395577df9a306ce1fa27d1d62b15528d9fc2e2
xubeixiong <44999304+xubeixiong@users.noreply.github.com>
2020-09-25 12:32:15 +0800

    Update post_data_to_mongo.sh

--- post_data_to_mongo.sh
+++ post_data_to_mongo.sh
@@ -1,43 +1,54 @@
 #!bin/bash
+#* */2 * * *
+MONGO_URL='mongodb://root:root123@192.168.8.8:27017'
+MONGO_DATABASE=defipluse
+MONGO_CELLECTION=marketdata
+TARGET_FILE=/opt/defipluse_data_to_mongo
+
 
 echo "$(date +%Y%-m-%d-%H:%M:%S)"
-cd /opt/chart_banck_mongo
-source ./env/bin/activate
+cd $TARGET_FILE
+source env/bin/activate
 
 echo ">>>> Enter virtual environment..."
 
-defi_data=$(curl "https://data-api.defipulse.com/api/v1/defipulse/api/MarketData?api-key=c5955059491aa04d18671faf1b8d8471ab4a09a19a80887c56f84dd35c1b")
+#获取指定网站数据
+# run_usa自定义命令，只接收一个参数
+defi_data=$(run_usa ' curl -s "https://data-api.defipulse.com/api/v1/defipulse/api/MarketData?api-key=93c078480f89a7fa220f2b91a7244ea17b5bab77e3cff6b0fa1e2d0ed22c" ')
+
 
 echo ">>>> query api data..."
 
+# 通过长度校验数据合法性
 len_data=${#defi_data}
 
 if (( $len_data < 3 ))
+
 then
-    echo ">>>> json too short: " $defi_data
-    ./deactivate
-    exit 1
+	echo ">>>> json too short: " $defi_data
+	deactivate
+	exit 1
 else
-    echo ">>>> json check OK. Go on."
-fi
+	echo ">>>> json check OK. Go on."
+	fi
 
-
+# 获取当前时间戳
 date_num=$(date +%s)
 
-
+# 利用jq加工目标数据，增加额外时间信息
 defipluse_data=$(echo $defi_data | jq -c --arg date_num $date_num '{time: $date_num} + .')
 
 
-export LC_ALL=en_US.UTF-8
-export MONGOCAT_URL='mongodb://root:root123@192.168.8.8:27017'
+#export LC_ALL=en_US.UTF-8
+# 创建mongo连接
+export MONGOCAT_URL=$MONGO_URL
 
 echo ">>>> save data to mongo..."
 
-
-mongocat -W -d defipluse marketdata -p json <<EOF
+# 以json格式将数据写入mongo
+mongocat -W -d $MONGO_DATABASE $MONGO_CELLECTION -p json <<EOF
 ${defipluse_data}
 EOF
 
-./deactivate
+deactivate
 echo ">>>> Exit virtual environment..."
-
